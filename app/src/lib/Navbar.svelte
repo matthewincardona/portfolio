cecfe46b8662889e5b7c9435d2b45170192d3a78
Matthew Incardona <doublethink.inc@gmail.com>
2024-08-02 11:24:18 -0400

    General fixes

--- app/src/lib/Navbar.svelte
+++ app/src/lib/Navbar.svelte
@@ -1,35 +1,42 @@
 <script>
 	import resume from '$lib/assets/Matthew_Incardona_Resume-UX-Engineer.pdf';
 
-	function toggleMobileNav() {
-		var x = document.getElementById('myTopnav');
-		if (x.className === 'topnav') {
-			x.className += ' responsive';
-		} else {
-			x.className = 'topnav';
-		}
-	}
+	// function toggleMobileNav() {
+	// 	var x = document.getElementById('myTopnav');
+	// 	if (x.className === 'topnav') {
+	// 		x.className += ' responsive';
+	// 	} else {
+	// 		x.className = 'topnav';
+	// 	}
+	// }
 
-	function closeMobileNav() {
-		var x = document.getElementById('myTopnav');
-		x.className = 'topnav';
-	}
+	// function closeMobileNav() {
+	// 	var x = document.getElementById('myTopnav');
+	// 	x.className = 'topnav';
+	// }
 </script>
 
 <nav>
-	<!-- Responsive navbar -->
+	<!-- Responsive navbar
 	<div class="topnav" id="myTopnav">
 		<div class="topnav__inner">
 			<a href="/" class="active" on:click={closeMobileNav}>Home</a>
-			<!-- svelte-ignore a11y-invalid-attribute -->
 			<a href="/about" on:click={closeMobileNav}>About</a>
-			<!-- <a href="/contact" on:click={closeMobileNav}>Contact Me</a> -->
 			<a href={resume} on:click={closeMobileNav}>Resume</a>
 		</div>
-		<!-- svelte-ignore a11y-invalid-attribute -->
 		<a href="" class="icon" on:click={toggleMobileNav}>
 			<i class="fa fa-bars" />
 		</a>
+	</div> -->
+
+	<div class="topnav" id="myTopnav">
+		<div class="topnav__inner">
+			<a href="/" class="active">Home</a>
+			<!-- svelte-ignore a11y-invalid-attribute -->
+			<a href="/about">About</a>
+			<!-- <a href="/contact" on:click={closeMobileNav}>Contact Me</a> -->
+			<a href={resume}>Resume</a>
+		</div>
 	</div>
 </nav>
 
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -19,26 +19,36 @@
 		// // start the application and load the scene
 		const spline = new Application(canvas);
 		spline.load('https://prod.spline.design/KWvZ9DsHNqCoOsAC/scene.splinecode').then(() => {
+			// Hide overlay
+			document.body.style.overflow = 'hidden';
+			const overlay = document.getElementsByClassName('.loader-container')[0];
+			document.body.style.overflow = 'auto'; // Restore scrolling
+			document
+				.getElementsByClassName('loader-container')[0]
+				.classList.add('loader-container--hidden');
+			// overlay.style.display = 'none';
+
 			setTimeout(function () {
 				spline.stop();
 			}, 2800);
 		});
-
-		// Hide overlay after 1 second
-		document.body.style.overflow = 'hidden';
-		setTimeout(function () {
-			const overlay = document.querySelector('.overlay');
-			document.body.style.overflow = 'auto'; // Restore scrolling
-			overlay.style.display = 'none';
-		}, 800);
 	});
 </script>
 
 <svelte:head>
-	<title>Matthew Incardona | UX Engineer</title>
+	<title>Matthew Incardona</title>
+	<link
+		rel="preload"
+		href="https://prod.spline.design/KWvZ9DsHNqCoOsAC/scene.splinecode"
+		as="script"
+	/>
 </svelte:head>
 
-<div class="overlay" />
+<!-- <div class="overlay" /> -->
+
+<div class="loader-container">
+	<div class="loader" />
+</div>
 
 <canvas id="canvas3d" />
 
@@ -68,15 +78,12 @@
 				</div> -->
 				<div />
 				<div>
-					<p>Years of experience in:</p>
+					<p>With years of experience in:</p>
 					<ul>
-						<li>Cloud Computing</li>
-						<li>JavaScript</li>
-						<li>HTML</li>
-						<li>CSS</li>
+						<li>AWS, Cloudflare, GCP</li>
+						<li>CSS, Tailwind</li>
 						<li>React, Svelte, Angular</li>
-						<li>Figma</li>
-						<li>Photoshop, Illustrator</li>
+						<li>Figma, Adobe Suite</li>
 					</ul>
 				</div>
 			</div>
@@ -270,30 +277,110 @@
 </section>
 
 <style>
-	.overlay {
-		position: fixed; /* Fix to viewport */
+	.loader-container {
+		width: 100vw;
+		height: 120dvh;
+		z-index: 100000000;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: #000000;
+		position: absolute;
+	}
+
+	:global(.loader-container--hidden) {
+		animation: fade-out 0.3s 0s both;
+	}
+
+	@keyframes fade-out {
+		0% {
+			opacity: 1;
+		}
+		99% {
+			opacity: 0;
+		}
+		100% {
+			visibility: hidden;
+		}
+	}
+
+	.loader {
+		--s: 25px;
+
+		--_d: calc(0.353 * var(--s));
+		width: calc(var(--s) + var(--_d));
+		aspect-ratio: 1;
+		display: flex;
+	}
+	.loader:before,
+	.loader:after {
+		content: '';
+		flex: 1;
+		clip-path: polygon(
+			var(--_d) 0,
+			100% 0,
+			100% calc(100% - var(--_d)),
+			calc(100% - var(--_d)) 100%,
+			0 100%,
+			0 var(--_d)
+		);
+		background: conic-gradient(
+			from -90deg at calc(100% - var(--_d)) var(--_d),
+			#fff 135deg,
+			#666 0 270deg,
+			#aaa 0
+		);
+		animation: l5 1.2s infinite;
+	}
+	.loader:before {
+		margin-right: calc(var(--_d) / -2 - 1px);
+	}
+	.loader:after {
+		margin-left: calc(var(--_d) / -2 - 1px);
+		animation-delay: 0.6s;
+	}
+	@keyframes l5 {
+		0% {
+			transform: translateY(0);
+		}
+		16.67% {
+			transform: translateY(-10px);
+		}
+		33.33% {
+			transform: translateY(10px);
+		}
+		50%,
+		100% {
+			transform: translateY(0);
+		}
+	}
+
+	/* .overlay {
+		position: fixed;
 		top: 0;
 		left: 0;
 		width: 100%;
 		height: 100%;
-		background-color: rgba(27, 11, 11, 0); /* Semi-transparent background */
-		z-index: 10; /* Ensure it's on top */
-	}
+		background-color: rgba(27, 11, 11, 0);
+		z-index: 10;
+	} */
+
 	#canvas3d {
 		position: absolute;
-		width: 100vw !important;
-		max-height: 100vh !important;
+		width: 120vw !important;
+		height: 120dvh !important;
 	}
 
 	.hero {
 		/* user-select: none; */
-		min-height: 100dvh;
+		min-height: 120dvh;
 		padding: 0% 2%;
 		position: relative;
 		overflow: hidden;
 		padding: 6%;
 		margin-top: -2%;
-		animation: fade-in 0.3s 0.5s ease-in-out both;
+		/* animation: fade-in 0.3s 0.5s ease-in-out both; */
+		box-shadow: inset 0px 0px 30px -10px rgba(31, 25, 25, 0.249);
 	}
 
 	@keyframes fade-in {
@@ -314,7 +401,7 @@
 	@media (max-width: 768px) {
 		.hero {
 			padding-top: 15%;
-			min-height: 80dvh;
+			min-height: 100dvh;
 		}
 	}
 
@@ -340,10 +427,6 @@
 	h1 {
 		transition: letter-spacing 0.8s cubic-bezier(0.77, 0.15, 0.07, 0.85);
 		white-space: nowrap;
-	}
-
-	.hero h1 {
-		user-select: none;
 	}
 
 	/* .hero__descr {
