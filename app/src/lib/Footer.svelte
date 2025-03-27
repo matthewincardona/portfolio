e516e1e8b28113bf0ab60b4cf08808171c7077f9
Matthew Incardona <doublethink.inc@gmail.com>
2025-03-26 23:19:36 -0400

    chore: remove unused code

--- app/src/lib/Footer.svelte
+++ app/src/lib/Footer.svelte
@@ -1,16 +1,9 @@
 <script>
 	import linkedinIcon from '$lib/assets/linkedin icon.svg';
-	import behanceIcon from '$lib/assets/behance icon.svg';
 </script>
 
 <footer>
 	<div class="footer-col">
-		<!-- <a href="https://www.behance.net/matthewincardo/" target="_blank" rel="noopener noreferrer">
-			<div class="footer__icon-row">
-				<img class="footer__icon" src={behanceIcon} alt="behance icon" />
-				<p>See more of my work on Behance</p>
-			</div>
-		</a> -->
 		<a
 			href="https://www.linkedin.com/in/matthew-incardona/"
 			target="_blank"
