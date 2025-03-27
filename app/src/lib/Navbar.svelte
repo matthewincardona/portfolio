9f37aaa108d51d40ef4c1da6db45ac68dc6413ec
Matthew Incardona <doublethink.inc@gmail.com>
2025-03-26 23:11:50 -0400

    chore: remove unused code

--- app/src/lib/Navbar.svelte
+++ app/src/lib/Navbar.svelte
@@ -1,35 +1,9 @@
 <script>
 	import resume from '$lib/assets/Matthew_Incardona_Resume.pdf';
 	import pdfIcon from '$lib/assets/pdf-icon.svg';
-
-	// function toggleMobileNav() {
-	// 	var x = document.getElementById('myTopnav');
-	// 	if (x.className === 'topnav') {
-	// 		x.className += ' responsive';
-	// 	} else {
-	// 		x.className = 'topnav';
-	// 	}
-	// }
-
-	// function closeMobileNav() {
-	// 	var x = document.getElementById('myTopnav');
-	// 	x.className = 'topnav';
-	// }
 </script>
 
 <nav>
-	<!-- Responsive navbar
-	<div class="topnav" id="myTopnav">
-		<div class="topnav__inner">
-			<a href="/" class="active" on:click={closeMobileNav}>Home</a>
-			<a href="/about" on:click={closeMobileNav}>About</a>
-			<a href={resume} on:click={closeMobileNav}>Resume</a>
-		</div>
-		<a href="" class="icon" on:click={toggleMobileNav}>
-			<i class="fa fa-bars" />
-		</a>
-	</div> -->
-
 	<div class="topnav" id="myTopnav">
 		<div class="topnav__inner">
 			<a href="/" class="active">Home</a>
@@ -45,8 +19,6 @@
 </nav>
 
 <style>
-	/* https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_topnav */
-
 	/* Style navbar */
 	.topnav {
 		position: fixed;
@@ -57,7 +29,6 @@
 		font-family: 'IBM Plex Sans', sans-serif;
 		color: var(--header-color);
 		padding: 5px 6%;
-		/* background-color: #00000010; */
 		backdrop-filter: blur(4px);
 	}
 
@@ -98,49 +69,4 @@
 	.topnav__inner-resume:hover {
 		opacity: 0.6;
 	}
-
-	/* .topnav .icon {
-		display: none;
-	} */
-
-	/* i,
-	.fa,
-	.fa-bars,
-	a.icon {
-		font-size: 25px;
-	} */
-
-	/* Apply this to everything but the first child of .topnav a */
-	/* @media screen and (max-width: 1000px) {
-		.topnav a {
-			display: none;
-		}
-
-		.topnav a.icon {
-			float: right;
-			display: block;
-		}
-
-		.topnav {
-			padding-left: 10px;
-		}
-	}
-
-	@media screen and (max-width: 1000px) {
-		.topnav.responsive {
-			display: flex;
-			flex-direction: row;
-			justify-content: space-between;
-		}
-
-		.topnav.responsive .icon {
-			position: relative;
-		}
-
-		.topnav.responsive a {
-			float: none;
-			display: block;
-			text-align: left;
-		}
-	} */
 </style>
