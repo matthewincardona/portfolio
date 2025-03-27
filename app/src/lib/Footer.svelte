a1b51c788ac6599c1188d5ff65e1cb32232e4a2d
Matthew Incardona <doublethink.inc@gmail.com>
2025-03-26 23:33:21 -0400

    style: tigthen footer spacing and remove extra text

--- app/src/lib/Footer.svelte
+++ app/src/lib/Footer.svelte
@@ -3,83 +3,25 @@
 </script>
 
 <footer>
-	<div class="footer-col">
-		<a
-			href="https://www.linkedin.com/in/matthew-incardona/"
-			target="_blank"
-			rel="noopener noreferrer"
-		>
-			<div class="footer__icon-row">
-				<img class="footer__icon" src={linkedinIcon} alt="linkedin icon" />
-				Connect with me on LinkedIn
-			</div>
-		</a>
-	</div>
-	<div class="footer-col">
-		<a href="mailto:matthew@douxdolci.com"
-			><button class="footer-btn">matthew@douxdolci.com</button></a
-		>
-	</div>
+	<a href="https://www.linkedin.com/in/matthew-incardona/" target="_blank">
+		<i class="fa-brands fa-linkedin" />
+	</a>
+	<a href="mailto:matthew@douxdolci.com" style="margin-top: 3px;">matthew@douxdolci.com</a>
 </footer>
 
 <style>
 	footer {
+		margin-top: 60px;
 		display: flex;
 		flex-direction: row;
-		flex-wrap: wrap;
-		justify-content: center;
-		align-content: center;
-		border-top: 4px solid var(--accent-color);
-		min-height: 200px;
+		place-content: center;
+		border-top: 2px solid var(--accent-color);
 		width: 100%;
-		padding-top: 10px;
-		margin-top: 60px;
+		padding: 24px 0;
+		gap: 2em;
 	}
 
-	.footer-col {
-		margin: 30px 60px;
-		display: grid;
-		grid-template-rows: auto;
-		row-gap: 0.5;
-		margin-top: auto;
-		margin-bottom: auto;
-	}
-
-	.footer-col button {
-		margin-top: 20px;
-		margin-bottom: 20px;
-	}
-
-	.footer-btn {
-		border: var(--accent-color) 3px solid;
-		background-color: #00000000;
-		padding: 8px 20px;
-		border-radius: 4px;
-		cursor: pointer;
-		transition: background-color ease-in 0.15s;
-		font-size: clamp(1rem, 2.5vw, 1rem);
-	}
-
-	.footer-btn:hover {
-		background-color: #0000001d;
-	}
-
-	.footer__icon-row {
-		display: grid;
-		grid-template-columns: 0.2fr 1fr;
-		grid-template-rows: auto;
-		column-gap: 1em;
-		align-items: center;
-		/* padding-bottom: 20px; */
-		transition: opacity 0.15s ease-in;
-	}
-
-	.footer__icon-row:hover {
-		opacity: 0.8;
-	}
-
-	.footer__icon {
-		width: 3em;
-		height: auto;
+	footer i {
+		font-size: 32px;
 	}
 </style>
