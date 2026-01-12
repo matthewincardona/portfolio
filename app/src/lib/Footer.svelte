16e704022f40a9525e15aa46434db998e6289bd6
Matthew Incardona <doublethink.inc@gmail.com>
2026-01-12 17:04:04 -0500

    update footer links

--- app/src/lib/Footer.svelte
+++ app/src/lib/Footer.svelte
@@ -1,12 +1,23 @@
 <script>
 	import LogoLinkedin from 'carbon-icons-svelte/lib/LogoLinkedin.svelte';
+	import LogoGithub from 'carbon-icons-svelte/lib/LogoGithub.svelte';
+	import Email from 'carbon-icons-svelte/lib/Email.svelte';
 </script>
 
 <footer>
 	<a href="https://www.linkedin.com/in/matthew-incardona/" target="_blank">
 		<LogoLinkedin size="40px" style="fill: var(--body-color)" />
 	</a>
-	<a href="mailto:matthew@douxdolci.com" style="margin-top: 7px;">matthew@douxdolci.com</a>
+	<a href="https://github.com/matthewincardona/501-peer-feedback-system" target="_blank">
+		<LogoGithub size="40px" style="fill: var(--body-color)" />
+	</a>
+
+	<a href="mailto:matthew@douxdolci.com" target="_blank">
+		<Email size="40px" style="fill: var(--body-color)" />
+	</a>
+	<a href="https://exploration.matthewincardona.com/" target="_blank" style="margin-top: 7px;"
+		>Explore More Work</a
+	>
 </footer>
 
 <style>
